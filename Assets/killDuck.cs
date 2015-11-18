4bde56c0cf31ad3177e97935e7ba234dcaaae3e8
jeffcarbine <jeffcarbine@gmail.com>
2015-11-18 13:47:58 -0700

    Made the duck killable!

---
+++ Assets/killDuck.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killDuck : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider hit) {
+		if(hit.tag == "killzone") {
+			Destroy (this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/spawnDuck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnDuck : MonoBehaviour {
+
+	public GameObject duck;
+
+	public void spawn() {
+
+		Instantiate (duck, transform.position, Quaternion.identity);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
