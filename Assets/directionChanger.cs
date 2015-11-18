5a32a01dd33307a356c63384a5af7baba9cb93e5
jeffcarbine <jeffcarbine@gmail.com>
2015-11-18 15:54:59 -0700

    Ducks now move and bounce around the game area.

---
+++ Assets/directionChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class directionChanger : MonoBehaviour {
+
+	public enum Changer {Horizontal, Vertical};
+	public Changer changer;
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+	void OnCollisionEnter(Collision hit) {
+		if (hit.transform.tag == "Duck") {
+			moveDuck movement = hit.gameObject.GetComponent<moveDuck>();
+			if(changer == Changer.Horizontal) {
+				movement.DirectionChanger (new Vector3(-1,1,0));
+			} else if (changer == Changer.Vertical) {
+				movement.DirectionChanger (new Vector3(1,-1,0));
+			}
+		}
+	}
+}
---
+++ Assets/moveDuck.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveDuck : MonoBehaviour {
+
+	public float speed;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		RandomDirection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + (direction * speed);
+	}
+
+	public void RandomDirection() {
+		direction = new Vector3 (Random.Range (-1f, 1f), Random.Range (.2f, 1f), 0);
+	}
+
+	public void DirectionChanger(Vector3 _dir) {
+		direction = new Vector3(direction.x * _dir.x, direction.y * _dir.y, 0);
+	}
+}
