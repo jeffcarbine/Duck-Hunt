5f6335de6d8a611b325e823108f8d4fd18adabc7
jeffcarbine <jeffcarbine@gmail.com>
2015-12-07 14:49:14 -0700

    Animations
    Duck and dog are animated.

--- Assets/killDuck.cs
+++ Assets/killDuck.cs
@@ -3,19 +3,20 @@ using System.Collections;
 
 public class killDuck : MonoBehaviour {
 
-	public void OnTriggerEnter(Collider hit) {
-		if(hit.tag == "killzone") {
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+	void OnTriggerEnter(Collider hit) {
+		if(hit.tag == "killZoneTag") {
 			Destroy (this.gameObject);
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
